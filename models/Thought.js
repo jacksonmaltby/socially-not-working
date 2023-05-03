205bec5e46bc5b456132148aedfeff224d25a765
J Maltby <jacksonmaltby@gmail.com>
2023-05-02 18:03:00 -0700

    Copied over mini project as baseline.

---
+++ models/Thought.js
@@ -0,0 +1,76 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create a course model
+const courseSchema = new Schema(
+  {
+    courseName: {
+      type: String,
+      required: true,
+    },
+    inPerson: {
+      type: Boolean,
+      default: true,
+    },
+    startDate: {
+      type: Date,
+      default: Date.now(),
+    },
+    endDate: {
+      type: Date,
+      // Sets a default value of 12 weeks from now
+      default: () => new Date(+new Date() + 84 * 24 * 60 * 60 * 1000),
+    },
+    students: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Student',
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+const Course = model('course', courseSchema);
+
+module.exports = Course;
+
+
+const { Schema, Types } = require('mongoose');
+
+const assignmentSchema = new Schema(
+  {
+    assignmentId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    assignmentName: {
+      type: String,
+      required: true,
+      maxlength: 50,
+      minlength: 4,
+      default: 'Unnamed assignment',
+    },
+    score: {
+      type: Number,
+      required: true,
+      default: () => Math.floor(Math.random() * (100 - 70 + 1) + 70),
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+module.exports = assignmentSchema;
