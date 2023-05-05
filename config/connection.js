093f9c2841c64910a70f0f53b4553e6296046480
J Maltby <jacksonmaltby@gmail.com>
2023-05-04 20:43:33 -0700

    Fixed MongoDB connection string.

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const { connect, connection } = require('mongoose');
 
-connect('mongodb:/localhost/postsTags', {
+connect('mongodb://localhost/postsTags', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
