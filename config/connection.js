d36fc4f991c79a63ab529af307213d55eebd5482
J Maltby <jacksonmaltby@gmail.com>
2023-05-08 19:36:23 -0700

    Fixed database connection.

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,8 @@
 const { connect, connection } = require('mongoose');
 
-connect('mongodb://localhost/postsTags', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+connect('mongodb://127.0.0.1:27017/socialDb', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
 
 module.exports = connection;
